c5b4bacb6bc56419f294a131874eda372187ce84
WednesdaySP <shashiprabha2737@gmail.com>
2024-11-22 23:11:18 +0530

    firebase integrated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 // ignore: unused_import
 //import 'package:appcontainer/src/home/search.dart';
+import 'package:appcontainer/firebase_options.dart';
 import 'package:appcontainer/src/home/BlueFamily.dart';
 import 'package:appcontainer/src/home/HomePage.dart';
 import 'package:appcontainer/src/home/PlayListScreen.dart';
 import 'package:appcontainer/src/home/RecentPlayScreen.dart';
 import 'package:appcontainer/src/home/SongScreen.dart';
 import 'package:appcontainer/src/home/Splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 // ignore: unused_import
 // import 'package:appcontainer/src/home/MusicApi_screen.dart';
@@ -14,7 +16,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 //import 'package:screens/screens.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp(
+   options: DefaultFirebaseOptions.currentPlatform,
+ );
   runApp(const MyApp());
 }
 
